956bcf36ea1734a537935fd45be91aaff6126fb2
iamrossan <roodhikari@gmail.com>
2021-11-23 11:56:42 +0545

    AppBar done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +15,26 @@ class VisionTestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          "कृपया ड्राइभिङ परीक्षणको लागि आफ्नो दृष्टि जाँच गर्नुहोस्",
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text(
+            "Roshan",
+          )
+        ],
+      )),
+    );
   }
 }
