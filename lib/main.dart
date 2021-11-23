b960ad70692105c78a11271e58c7e59a065f5672
iamrossan <roodhikari@gmail.com>
2021-11-23 13:24:44 +0545

    visionTest Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
+import 'dart:convert';
 import 'dart:ui';
 
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runnApp:
   runApp(const MaterialApp(
     home: VisionTestScreen(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class VisionTestScreen extends StatefulWidget {
+class VisionTestScreen extends StatelessWidget {
   const VisionTestScreen({Key? key}) : super(key: key);
 
   @override
-  State<VisionTestScreen> createState() => _VisionTestScreenState();
-}
-
-class _VisionTestScreenState extends State<VisionTestScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
         title: const Text(
-          "कृपया ड्राइभिङ परीक्षणको लागि आफ्नो दृष्टि जाँच गर्नुहोस्",
+          "दृष्टि जाँच गर्नुहोस्",
           style: TextStyle(
             fontSize: 18,
           ),
@@ -33,12 +32,110 @@ class _VisionTestScreenState extends State<VisionTestScreen> {
         backgroundColor: Colors.deepPurple,
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text(
-              "Roshan",
-            ),
-          ],
+        child: FutureBuilder(
+          future: rootBundle.loadString("assets/json/visionTest.json"),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var data = jsonDecode(snapshot.data.toString());
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: const BoxDecoration(),
+                child: ListView.builder(
+                  itemCount: data.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text(
+                            "उत्तर जान्न फोटोमा किल्क गर्नुहोस्।",
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.deepPurple,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                    return FlipCard(
+                      direction: FlipDirection.HORIZONTAL,
+                      front: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 250,
+                              // width: MediaQuery.of(context).size.width,
+                              // height: MediaQuery.of(context).size.height,
+                              decoration: const BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    blurRadius: 6,
+                                    color: Colors.grey,
+                                    offset: Offset(2, 8),
+                                  )
+                                ],
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              "assets/images/vision/" +
+                                  data[index - 1]["image"],
+                              fit: BoxFit.fill,
+                            ),
+                          )
+                        ],
+                      ),
+                      back: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 250,
+                            decoration: const BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 8,
+                                  color: Colors.grey,
+                                  offset: Offset(8, 15),
+                                )
+                              ],
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text(
+                              data[index - 1]["name"],
+                              style: const TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       ),
     );
